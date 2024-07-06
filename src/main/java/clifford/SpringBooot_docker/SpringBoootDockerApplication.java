d39cbee6a9a221f55eb105cac5decff4b6109ab5
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2024-07-06 01:39:14 +0000

    initial commit

---
+++ src/main/java/clifford/SpringBooot_docker/SpringBoootDockerApplication.java
@@ -0,0 +1,13 @@
+package clifford.SpringBooot_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoootDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clifford/SpringBooot_docker/SpringBoootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package clifford.SpringBooot_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
