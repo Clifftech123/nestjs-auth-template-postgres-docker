9f96950d52611013cf345e8f6b828fc090332ce8
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2024-08-13 23:36:20 +0000

    Remove unnecessary files and code

--- src/main/java/clifford/SpringBooot_docker/Exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package clifford.SpringBooot_docker.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-}
-
--- src/main/java/clifford/SpringBooot_docker/Repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package clifford.SpringBooot_docker.Repository;
-
-import clifford.SpringBooot_docker.Entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
